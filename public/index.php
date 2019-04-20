99be81d95242002e2c919c378b954507d667e6c7
Loctarogar <igrsl1990@gmail.com>
2019-04-21 00:33:16 +0300

    Simple autoload

--- public/index.php
+++ public/index.php
@@ -2,11 +2,28 @@
 
 $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
+//constant for current directory
+define('WWW', __DIR__);
+//constant for 'core' directory
+define('CORE', dirname(__DIR__).'/vendor/core');
+//'root' directory
+define('ROOT', dirname(__DIR__));
+//'app' directory
+define('APP', dirname(__DIR__).'/app');
+
 require '../vendor/core/Router.php';
 require '../vendor/libs/functions.php';
-require '../app/controllers/Main.php';
-require '../app/controllers/Posts.php';
-require '../app/controllers/PostsNew.php';
+
+//require '../app/controllers/Main.php';
+//require '../app/controllers/Posts.php';
+//require '../app/controllers/PostsNew.php';
+
+spl_autoload_register(function ($class) {
+    $file = APP."/controllers/$class.php";
+    if(is_file($file)){
+        require_once $file;
+    }
+});
 
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
