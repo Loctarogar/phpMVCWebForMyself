2e5c6e9c3178b10b79cf2ddbdeea7b94c608116e
Loctarogar <igrsl1990@gmail.com>
2019-05-04 23:34:10 +0300

    Errors. test\error\index.php

---
+++ public/test/error/index.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ *
+ *
+ */
+
+// this CONST has value 1 to show all errors. so called development mod
+// i can change it to 0, to suppress errors. production mod
+ini_set('display_errors', 1);
+define("DEBUG", 1);
+
+class ErrorHandler{
+    public function __construct()
+    {
+        if(DEBUG){
+            error_reporting(-1);
+        }else{
+            error_reporting(0);
+        }
+
+        // вказуемо функцію для обробки помилок
+        set_error_handler([$this, "errorHandler"]);
+        ob_start();
+        register_shutdown_function([$this, "fatalErrorHandler"]);
+    }
+
+    public function errorHandler($errno, $errstr, $errfile, $errline){
+        //var_dump($errno, $errstr, $errfile, $errline);
+        $this->displayError($errno, $errstr, $errfile, $errline);
+        return true;
+    }
+
+    public function fatalErrorHandler(){
+        $error = error_get_last();
+        if( !empty($error) AND $error['type'] & (E_ERROR | E_PARSE | E_COMPILE_ERROR | E_CORE_ERROR) ){
+            ob_end_clean();
+            $this->displayError($error['type'], $error['message'], $error['file'], $error['line']);
+        }else{
+            ob_end_flush();
+        }
+    }
+
+    protected function displayError($errno, $errstr, $errfile, $errline, $response = 500){
+        http_response_code($response);
+        if(DEBUG){
+            require 'views/dev.php';
+        }else{
+            require 'views/prod';
+        }
+        die;
+    }
+}
+
+new ErrorHandler();
+
+test();
+echo $test;
